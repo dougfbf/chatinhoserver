81ea5c961735ed71a0b97fb3b96037bb45fe2763
Douglas Farias <123842050+dougfbf@users.noreply.github.com>
2023-05-15 20:55:32 -0400

    bug corrigido

--- index.js
+++ index.js
@@ -10,24 +10,39 @@ const port = process.env.PORT || 3000
 let connectedUsers = []
 let messages = []
 
+function getDate() {
+    let date = new Date()
+    date.setHours(date.getHours())
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    const second = date.getSeconds().toString().padStart(2, '0');
+    return `${hour}:${minute}:${second}`
+}
+
 io.on('connection', (socket) => {
     let user
     io.emit('messagesUpdate', messages)
     socket.on('clientConnection', (data) => {
         user = {name: data.user}
+        messages.push('message', {user: user.name, type: 'joined', date: getDate()})
+        io.emit('messagesUpdate', messages)
         connectedUsers.push(user)
         console.log(`${user.name} entrou!`)
         console.log(connectedUsers)
         io.emit('usersUpdate', connectedUsers)
     })
     socket.on('message', (data) => {
+        console.log(data)
         messages.push(data)
         io.emit('messagesUpdate', messages)
+        console.log(messages)
     })
     socket.on('disconnect', () => {
         connectedUsers = connectedUsers.filter(item => item.name !== user.name)
         console.log(`${user.name} saiu!`)
         console.log(connectedUsers)
+        messages.push('message', {user: user.name, type: 'left', date: getDate()})
+        io.emit('messagesUpdate', messages)
         io.emit('usersUpdate', connectedUsers)
     })
 })
