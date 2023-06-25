93f6a92cf501658c99c1a40b076280a05f85780e
Douglas Farias <123842050+dougfbf@users.noreply.github.com>
2023-06-25 19:03:23 -0400

    burro

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const options = {
     dbName: 'Chatinho'
 }
 
-mongoose.connect('mongodb+srv://Chatinho:Wawdst7!@chatinho.0rkobbh.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Chatinho:Wawdst7!@chatinho.0rkobbh.mongodb.net/?retryWrites=true&w=majority', options)
 //mongoose.connect('mongodb://127.0.0.1:27017', options)
 const db = mongoose.connection
 
